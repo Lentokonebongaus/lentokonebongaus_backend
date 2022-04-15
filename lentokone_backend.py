046f5f1873fe251e2595a66b5c33b9333ed73dec
Eeli Killström <eeli.killstrom@gmail.com>
2022-04-15 20:27:32 +0300

    Plane pictures api

--- lentokone_backend.py
+++ lentokone_backend.py
@@ -6,7 +6,7 @@ import re
 import datetime
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import io
 
 #-------------------------------------------------#
@@ -163,7 +163,6 @@ def plane_in_planes_folder(manufacturer, model, owner):
             return True
     return False
 
-
 def get_newest_local_csv_path():
     cwd = os.getcwd()
     csv_folder_path = os.path.join(cwd,"csv")
@@ -187,6 +186,96 @@ def plane_data_compact(icao24):
     return plane_compact
 # --------------------------------------
 
+# ----------- Image scraper ------------
+
+#!/usr/bin/env python3
+
+import json
+import urllib.request, urllib.error, urllib.parse
+
+def get_soup(url,header):
+    return BeautifulSoup(urllib.request.urlopen(
+        urllib.request.Request(url,headers=header)),
+        'html.parser')
+
+def plane_exists_in_planes(manufacturer, model, owner):
+    file_name_manufacturer = manufacturer.replace(" ","_")
+    file_name_model = model.replace(" ", "_")
+    file_name_owner = owner.replace(" ", "_")
+    file_full_name = f"{file_name_manufacturer}+{file_name_model}+{file_name_owner}.jpg"
+    assert(os.path.exists("plane_imgs"))
+    plane_files = os.listdir("plane_imgs")
+    print(f"Plane files: {plane_files}")
+    print(f"New file: {file_full_name}")
+    for plane_file in plane_files:
+        if plane_file == file_full_name:
+            return True
+    return False
+
+def get_plane_image_path(manufacturer, model, owner):
+    cwd = os.getcwd()
+    planes_folder_path = os.path.join(cwd, "plane_imgs")
+    file_name_manufacturer = manufacturer.replace(" ","_")
+    file_name_model = model.replace(" ", "_")
+    file_name_owner = owner.replace(" ", "_")
+    file_full_name = f"{file_name_manufacturer}+{file_name_model}+{file_name_owner}.jpg"
+    assert(os.path.exists("plane_imgs"))
+    plane_files = os.listdir("plane_imgs")
+    for plane_file in plane_files:
+        if plane_file == file_full_name:
+            plane_file_img_path = os.path.join(planes_folder_path, plane_file)
+            print(f"Found plane file in {plane_file_img_path}")
+            return plane_file_img_path
+
+    return False
+
+
+def save_plane_image(manufacturer, model, owner):
+    file_name_manufacturer = manufacturer.replace(" ","_")
+    file_name_model = model.replace(" ", "_")
+    file_name_owner = owner.replace(" ", "_")
+    query = f"{file_name_manufacturer}+{file_name_model}+{file_name_owner}"
+    url="http://www.bing.com/images/search?q=" + query + "&FORM=HDRSC2"
+
+    DIR="plane_imgs"
+    header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    soup = get_soup(url,header)
+
+    ActualImages=[]
+    for a in soup.find_all("a",{"class":"iusc"}):
+        m = json.loads(a["m"])
+        murl = m["murl"]
+        turl = m["turl"]
+
+        image_name = urllib.parse.urlsplit(murl).path.split("/")[-1]
+
+        ActualImages.append((image_name, turl, murl))
+
+    if not os.path.exists(DIR):
+        os.mkdir(DIR)
+
+    file_name_manufacturer = manufacturer.replace(" ","_")
+    file_name_model = model.replace(" ", "_")
+    file_name_owner = owner.replace(" ", "_")
+    FILE_NAME = f"{file_name_manufacturer}+{file_name_model}+{file_name_owner}.jpg"
+
+    if not plane_exists_in_planes(manufacturer, model, owner):
+        print("Plane file name not in 'planes. Creating a new file...")
+        for i, (image_name, turl, murl) in enumerate(ActualImages):
+            # TODO: Remove unnecessary loop
+            if(i==0):
+                try:
+                    raw_img = urllib.request.urlopen(turl).read()
+                    f = open(os.path.join(DIR, FILE_NAME), 'wb')
+                    f.write(raw_img)
+                    f.close()
+                except Exception as e:
+                    print("could not load : " + image_name)
+                    print(e)
+    else:
+        print("Plane already in 'plane_imgs' folder")
+
+#save_plane_image("Airbus","ATR 72 500","Finnair")
 
 # -------- Flask API endpoint ----------
 @app.route("/")
@@ -198,10 +287,17 @@ def get_by_icao24():
     else:
         return jsonify({"ok":False})
 
+
 @app.route("/image")
 def get_image():
     manufacturer = request.args.get("manufacturer")
     model = request.args.get("model")
+    owner = request.args.get("owner")
+    if(not plane_exists_in_planes(manufacturer, model, owner)):
+        save_plane_image(manufacturer, model, owner)
+    plane_img_file_path = get_plane_image_path(manufacturer, model, owner)
+    return send_file(plane_img_file_path, mimetype='image/gif')
+    
 
 # --------------------------------------
 
